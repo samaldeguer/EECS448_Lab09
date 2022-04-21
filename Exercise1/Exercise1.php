75f32563d32ce91924c28cef18866df609c7eb56
Sam Aldeguer <aldeguersam@ku.edu>
2022-04-21 10:32:39 -0500

    Create Exercise1: Multiplication Table

--- Exercise1/Exercise1.php
+++ Exercise1/Exercise1.php
@@ -1,1 +1,22 @@
-
+<?php
+  echo "<table>";
+  echo "<tr>";
+  echo "<th>&nbsp;</th>";
+  for ($i = 1; $i <= 100; $i++)
+  {
+    echo "<td><strong>".$i."</strong></td>";
+  }
+  echo "</tr>";
+  for ($i = 1; $i <= 100; $i++)
+  {
+    echo "<tr>";
+    echo "<th>".$i."</th>";
+    for ($j = 1; $j <= 100; $j++)
+    {
+      $product = $i * $j;
+      echo "<td>".$product."</td>";
+    }
+    echo "</tr>";
+  }
+  echo "</table>";
+?>
