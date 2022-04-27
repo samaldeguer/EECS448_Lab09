749bf737151e5e80466e9f402cc49226267036b8
Sam Aldeguer <aldeguersam@ku.edu>
2022-04-26 23:01:51 -0500

    Create login

--- Exercise3/formChecker.js
+++ Exercise3/formChecker.js
@@ -1,1 +1,25 @@
+function validateEmail(email)
+{
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
 
+function checkLogin()
+{
+  if (validateEmail(document.getElementById("username").value) == false)
+  {
+      alert("Username must be an email (name@domain.com)");
+  }
+  else if (document.getElementById("password").value == null || document.getElementById("password").value == "")
+  {
+    alert("Must enter a password");
+  }
+  else
+  {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    document.getElementById("login").style.display = "none";
+    document.getElementById("shop").style.display = "block";
+    alert("Welcome to Sam's shoes, " + username + "!\nYour password is " + password);
+  }
+}
